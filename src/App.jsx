4d61b8df12f836a79603ce272918acc8604bacbb
sourabh pandey <57836486+PANDEY247@users.noreply.github.com>
2025-11-21 16:03:48 +0530

    Updated package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import './App.css'
-import { AuthProvider } from './context/AuthContext'
 import Navbar from './Components/Layout/Navbar'
 import HeroSection from './Components/Section/HeroSection'
 import Menu from './Components/Menu'
 import Gallery from './Components/Gallery'
 import Footer from './Components/Layout/Footer'
 import About from './Components/Section/About'
-
+// import Signup from './Components/Signup'
 function App() {
+
   return (
-    <AuthProvider>
+    <>
       <Navbar/>
+      {/* <Signup/> */}
       <HeroSection/>
       <About/>
       <Menu/>
       <Gallery/>
       <Footer/>
-    </AuthProvider>
+
+
+    </>
+    
+    
   )
 }
 
